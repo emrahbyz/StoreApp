bdb1446a58d5c3e886b29ad1d09aaaba0a4f7dd7
Emrah <146847947+emrahbyz@users.noreply.github.com>
2024-03-03 01:59:12 +0300

    store component bug fix

--- src/Store.jsx
+++ src/Store.jsx
@@ -1,33 +1,39 @@
-import React, { useState } from "react";
-
+import React from "react";
 import "./Store.css";
+
 const Store = ({ store, removeCard }) => {
-  const [showFullText, setshowFullText] = useState(false);
-
-  const toggleShowText = () => {
-    setshowFullText(!showFullText);
-  };
-
   return (
     <div className="mainContainer">
       {store.map((item) => (
-        <div className="storeContainer" key={item.id}>
-          <img src={item.image} alt={item.company} />
-          <div className="importInfo">
-            <h2> Name : {item.name} </h2>
-            <h2> Price : $ {item.price} </h2>
-          </div>
-          <p>
-            {showFullText
-              ? item.description
-              : `${item.description.slice(0, 233)}...`}
-            <button onClick={toggleShowText}>
-              {showFullText ? "Show less" : "Read More"}
-            </button>
-          </p>
-          <button onClick={() => removeCard(item.id)}>Remove</button>
-        </div>
+        <StoreItem key={item.id} item={item} removeCard={removeCard} />
       ))}
+    </div>
+  );
+};
+
+const StoreItem = ({ item, removeCard }) => {
+  const [showFullText, setShowFullText] = React.useState(false);
+
+  const toggleShowText = () => {
+    setShowFullText(!showFullText);
+  };
+
+  return (
+    <div className="storeContainer">
+      <img src={item.image} alt={item.company} />
+      <div className="importInfo">
+        <h2> Name : {item.name} </h2>
+        <h2> Price : $ {item.price} </h2>
+      </div>
+      <p>
+        {showFullText
+          ? item.description
+          : `${item.description.slice(0, 233)}...`}
+        <button onClick={toggleShowText}>
+          {showFullText ? "Show less" : "Read More"}
+        </button>
+      </p>
+      <button onClick={() => removeCard(item.id)}>Remove</button>
     </div>
   );
 };
